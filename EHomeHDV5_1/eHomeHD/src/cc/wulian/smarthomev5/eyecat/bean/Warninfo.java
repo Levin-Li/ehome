dfdc6d1fd56e652546124c9df9010077142ab56b
zcz123 <1473787180@qq.com>
2017-04-28 17:32:08 +0800

    报警列表

---
+++ EHomeHDV5_1/eHomeHD/src/cc/wulian/smarthomev5/eyecat/bean/Warninfo.java
@@ -0,0 +1,21 @@
+package cc.wulian.smarthomev5.eyecat.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/4/28.
+ */
+
+public class Warninfo implements Serializable {
+    String method;
+    String create;
+    int offset;
+    int limit;
+    int max;
+    String aid;
+    String time;
+    String alarmDevSn;
+    String fid;
+    String type;
+
+}
