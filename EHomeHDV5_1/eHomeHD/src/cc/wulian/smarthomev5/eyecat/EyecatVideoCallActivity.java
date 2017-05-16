f73c8e2b78fb0cd893681cb2021847a1ce795b10
wangtaogx <584692417@qq.com>
2017-05-17 01:01:29 +0800

    优化视频

--- EHomeHDV5_1/eHomeHD/src/cc/wulian/smarthomev5/eyecat/EyecatVideoCallActivity.java
+++ EHomeHDV5_1/eHomeHD/src/cc/wulian/smarthomev5/eyecat/EyecatVideoCallActivity.java
@@ -190,7 +190,7 @@ public class EyecatVideoCallActivity extends Activity {
 
 			public void surfaceDestroyed(SurfaceHolder holder) {
 				Log.i("eyecat:","surfaceDestroyed");
-//				hangUpCallNotCloseActivity();
+				hangUpCallNotCloseActivity();
 			}
 		});
 		if (instance.isPortrait()) {
